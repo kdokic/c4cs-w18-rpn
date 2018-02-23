c2bea67267548526a540b6c341360dff6741df3f
Kristijan Dokic <kdokic@umich.edu>
2018-02-23 10:49:16 -0500

    added test_mul

--- test_rpn.py
+++ test_rpn.py
@@ -14,3 +14,6 @@ class TestBasics(unittest.TestCase):
     def test_toomany(self):
         with self.assertRaises(TypeError):
              result = rpn.calculate('1 2 3 +')
+    def test_mul(self):
+        result = rpn.calculate('2 3 *')
+        self.assertEqual(6, result)
